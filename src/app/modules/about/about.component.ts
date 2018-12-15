73f0324d119db8121c23b8caca134a500ab495ba
praveen potnuru <praveenpotnuru@gmail.com>
2018-12-15 16:28:38 +0530

    modified

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.less']
+  styleUrls: ['./about.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AboutComponent implements OnInit {
   private fragment: string;
--- src/app/modules/gallery/gallery.component.ts
+++ src/app/modules/gallery/gallery.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.less']
+  styleUrls: ['./gallery.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GalleryComponent implements OnInit {
 
